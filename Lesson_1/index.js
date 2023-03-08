71f817351b2b81b9e7b4ff083e53b5e4615472fd
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-03-08 19:32:59 +0500

    JSga kirish

---
+++ Lesson_1/index.js
@@ -0,0 +1,83 @@
+
+// console.log("Hello js1"); //1 //jsda 1- amalni bajarib bolgandan keyin keyingi amalga o'tadi
+// console.log("Hello js2"); //2
+// console.log("Hello js3"); //3
+// console.log("Hello js4"); //4
+
+// let var const
+
+// let text = "Webbrain Academy";
+// let number = 721
+
+// console.log(text);
+// console.log(number);
+
+// var login = "webbrain academy"
+
+// var login; //declaer
+
+// console.log(login);
+
+// login = "webbrain academy"  //assign ->yani qiymat biriktirish
+
+// console.log(login);
+
+
+// var login = parol = 712;
+
+// var login=712;
+// var prol=712;
+
+
+// var login=712;
+// var parol=login
+
+// console.log(login);
+// console.log(parol);
+
+// console.log(parol,parol);
+ 
+    
+    // let text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At deserunt esse dolorem ducimus optio iste, aspernatur sequi fugiat porro iusto?"
+
+    // console.log(text);
+    // console.log(text);
+    // console.log(text);
+
+
+// var student = "null";
+// console.log(login);
+
+
+// let sm = Symbol()
+// console.log(sm);
+
+
+// objects arrays function
+
+// let obj = {login: "Webbrain", parol: 701 , fullname: 'Azizbek'};
+// let arr = [obj, obj, obj, obj, obj];
+
+// console.log(arr);
+
+
+
+// let num1 = 1
+// let num2 = 1
+// console.log(num1===num2);  true
+
+
+
+// let num1 = 3
+// let num2 = 1
+// console.log(num1===num2);   false
+
+
+// let a= 1
+// let b= '1'
+// let c = b
+// console.log(c);
+// console.log(a === b);
+// console.log(a == b);
+// console.log(b = a);
+
---
+++ Lesson_1/review.js
@@ -0,0 +1,52 @@
+// Js nima ?
+
+/// Js bu - dasturlash turi
+/// Js was  invented by Brendan Each in 1995
+/// 1- nomi LiveScript bo'lgan
+/// Keyinchalik 1997 yil Ecma nomiga o'zgargan
+/// Compiler..........-> js da yozilgan kodni yurgizish.
+
+
+// Variables
+
+/// Variable............-> MAlumotlarni saqlash uchun ishlatamiz -> (let var const)
+//// Let ...............-> malumot kerak bo'lmagan vaqtda ozi avtomatik malumotni memorydan ocirb yuboradi  ( let - skope    )
+//// Var................-> malumotlardan foydalanmagan vaqtimiz memorydan ochib ketmaydi va xotirada qoladi ( var - global   )
+//// const..............-> constanta yani keyinchalik boshqa qiymat biriktirb bolmaydi                      ( const -ozgarmas)
+/// Console.log();......-> malumotni chiqarib berish uchun ishlatiladi.
+/// Single thread.......-> Bir vaqtni o'zida faqat bitta amalni bajara oladigan dasturlash tili.
+
+// Variable ni neechi xil usulda declair(elon) qilsa boladi?
+/// 3 xil usulda
+/// (1- usul) var login = parol = email = 712;
+/// (2- usul) var login=712;
+///           var prol=712;
+/// (3-usul)  var login=712;
+///           var parol=login
+
+/// console.log("parol")
+
+
+// Data type
+
+/// Jsda 8 ta data type bor 
+//// Ulardan 7 tasi Primative ( Number, Bigint, String, Boolean, Null, Undefined, Symbols) 
+//// 1 tasi NuN Primative ( Objects )
+/// Objects ga ( objects, arrays, function ) kiradi
+//// obj.........-> {}
+//// array.......-> []
+//// function....-> (){}
+
+/// String........-> 3 xil korinishda yoziladi ya'ni -> " " , ' ' , ` `  (<-qo'shtrnoq)
+///
+/// Resorvt key words......-> jsda oldndan belgilab qoyilgan kalit sozlarga aytiladi (let, var, const, symbol,- hamma narsa kiradi)
+
+/// =   .......-> tenglab qoyadi
+/// ==  .......->taqqoslash (tengmi)
+/// === .......-> data type tengmi 
+///
+
+/// Slot......-> xotirada joylashishi
+//// Hip va stek
+//// Hip.......-> linklar joylashadi
+//// Stek.......-> valiu joylashadi

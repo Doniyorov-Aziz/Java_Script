700c5d972a6f91e2c54318de864023a689403ada
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-03-08 19:31:33 +0500

    Introduction

---
+++ Git-Hub/index.js
@@ -0,0 +1,31 @@
+
+// Karra jadvalini kodi
+
+// for (let i = 2; i < 100; i++) {
+//     console.log(`${i} - karra boshlandi :\n`);
+//     for (let j = 1; j < 100; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
+
+// Git Hub 
+
+/// Git Hub bu Dasturchilar uchun fileshka 
+//// Yani dasturchilar git hub orqali malumot ulashadi 
+
+/// master.................-> bu fleshkani asosiy o'zagi hisoblanadi.
+/// branch.................-> bu masterni ustida biror bir o'zgartirish qilmoqchi bolsa alohida ochiladigan kichik qism.
+/// pull...................-> bu biror branchdan ikkinchi branchga malumotlarni o'tqazish.
+/// push...................-> bu branchdagi malumotlarni Master ichiga qo'shish.
+
+/// remove.................-> malumotni o'chirish uchun ishlatiladi.
+
+/// 
+
+// for (let i = 0; i < 100000; i++) {
+//     if (i % 2 === 1) {
+//         console.log(i, `toq son`)
+//     } else {
+//         console.log(i, `juft son`);
+//     }
+// }

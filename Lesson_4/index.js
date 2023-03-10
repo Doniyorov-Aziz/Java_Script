efa09d037264f2823f84b062551c1b0f96836db3
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-03-10 19:54:53 +0500

    Switch | For loop.

--- Lesson_4/index.js
+++ Lesson_4/index.js
@@ -108,4 +108,43 @@
 // }
 // console.log(a,b,d);
 
+// 
 
+// let str = "Webbrain academy"
+// console.log(str[5]);
+
+// let i = 20
+// for (i; i >= 1; i--){
+//     console.log(i);
+// }
+
+// let a = 1
+// for (a; a <= 1000; a++) {
+//     console.log(a);
+// }
+
+
+
+
+// for (let a = 1; a <= 1000; a++) {
+//     console.log(a);
+// }
+
+// for(var i=1;i<=10; i=i+2){
+//     console.log(i);
+// }
+
+// let i = 1
+// for (; i <= 10; i++) {
+//     if (i % 2 === 0){
+//         console.log(i,"juft son");
+//     }else{
+//         console.log(i,"toq son");
+//     }
+// }
+
+// let i=1
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
--- Lesson_4/review.js
+++ Lesson_4/review.js
@@ -4,3 +4,7 @@
 // switch ifga alternativ sifatida ishlatida 
 // Case - bu holat yani o'zgaruvchini holati qanaqa bo'siin degani 
 // 
+
+// ForLoop..-> ittaratsiya yani aylantirish
+/// Loop....-> bitta narsani qayta qayta doimiy qilinishi yoki nimadur doimiy harakatda bo'lishi
+   

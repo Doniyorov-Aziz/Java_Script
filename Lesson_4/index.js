431054806dedbe44f45a290b988ad5959f82d667
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-03-10 17:56:41 +0500

    Switch | For loop.

---
+++ Lesson_4/index.js
@@ -0,0 +1,111 @@
+// let weather = "yomgir"
+// switch (weather){
+//     case "yomgir": console.log("bugun yomg'ir");
+//     break;
+// }
+
+// let son = "123"
+// switch (son){
+//     case "123":console.log("bir yuz yigirma uch");
+//     break;
+// }
+
+// let num = "698"
+// let y = parseInt(num / 100)
+// num = num % 100;
+// let o = parseInt(num / 10)
+// num = num % 10;
+// c = num
+// a = "" ; b = "" ; d = "";
+// switch (y) {
+//     case 1:
+//         a = "bir yuz";
+//         break;
+//     case 2:
+//         a = "ikki yuz";
+//         break;
+//     case 3:
+//         a = "uch yuz";
+//         break;
+//     case 4:
+//         a = "to'rt yuz";
+//         break;
+//     case 5:
+//         a = "besh yuz";
+//         break;
+//     case 6:
+//         a = "olti yuz";
+//         break;
+//     case 7:
+//         a = "yetti yuz";
+//         break;
+//     case 8:
+//         a = "sakkiz yuz";
+//         break;
+//     case 9:
+//         a = "to'qqiz yuz";
+//         break;
+// }
+// switch (o) {
+//     case 1:
+//         b = "o'n";
+//         break;
+//     case 2:
+//         b = "yigirma";
+//         break;
+//     case 3:
+//         b = "o'ttiz";
+//         break;
+//     case 4:
+//         b = "qiriq";
+//         break;
+//     case 5:
+//         b = "ellik";
+//         break;
+//     case 6:
+//         b = "oltmish";
+//         break;
+//     case 7:
+//         b = "yetmish";
+//         break;
+//     case 8:
+//         b = "sakson";
+//         break;
+//     case 9:
+//         b = "to'qson";
+//         break;
+
+// }
+// switch (c) {
+//     case 1:
+//         d = "bir";
+//         break;
+//     case 2:
+//         d = "ikki";
+//         break;
+//     case 3:
+//         d = "uch";
+//         break;
+//     case 4:
+//         d = "to'rt";
+//         break;
+//     case 5:
+//         d = "besh";
+//         break;
+//     case 6:
+//         d = "olti";
+//         break;
+//     case 7:
+//         d = "yetti";
+//         break;
+//     case 8:
+//         d = "sakkiz";
+//         break;
+//     case 9:
+//         d = "to'qqiz";
+//         break;
+
+// }
+// console.log(a,b,d);
+
+
---
+++ Lesson_4/review.js
@@ -0,0 +1,6 @@
+// false ni ifodalaydigan qiymatlar:......-> 0,null,false,undifined,""
+
+// Switch 
+// switch ifga alternativ sifatida ishlatida 
+// Case - bu holat yani o'zgaruvchini holati qanaqa bo'siin degani 
+// 

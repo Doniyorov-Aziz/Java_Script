aab15f4a54e433f50b94a0bb1b2005cf9add5014
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-03-11 13:47:54 +0500

    Switch | For loop.

---
+++ Lesson_4/homework.js
@@ -0,0 +1,41 @@
+// 
+// let browser = "Safari"
+// switch (browser) {
+//     case "Edge":
+//         console.log("You have got the Edge!");
+//         break;
+
+//     case "Firefox":
+//         console.log("Ok we support these Firefox");
+//         break;
+//     case "Safari":
+//     case "Chrome":
+//     case "Google":
+//         console.log("Ok we support these browsers too");
+//         break;
+//     default:
+//         console.log("We hope that this page looks ok!");
+// }
+
+// 
+
+// let i=7
+// while(i){
+//     console.log(i--);
+// }
+
+// 
+
+// let a = 2
+// if (a == 0) {
+//     console.log(0);
+// }
+// if (a == 1) {
+//     console.log(1);
+// }
+// if (a == 2) {
+//     console.log(2);
+// } 
+
+// 
+
